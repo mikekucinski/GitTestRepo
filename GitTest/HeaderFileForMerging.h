67ea88128c9185e03e9754a0aea45f74421280db
Michael Kucinski <engineermichigan@gmail.com>
2015-07-01 17:39:48 -0700

    Adding header file

---
+++ GitTest/HeaderFileForMerging.h
@@ -0,0 +1,13 @@
+//
+//  HeaderFileForMerging.h
+//  Pods
+//
+//  Created by Michael Kucinski on 7/1/15.
+//
+//
+
+#ifndef Pods_HeaderFileForMerging_h
+#define Pods_HeaderFileForMerging_h
+
+
+#endif
